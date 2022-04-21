4c9801e1b7c4165a94614f130f9ddd1239b8d647
koobawuah <me@bawuahboakye.com>
2022-04-21 01:41:18 +0000

    Initial update

---
+++ src/components/Link.js
@@ -0,0 +1,12 @@
+const Link = (props) => {
+
+    return (
+        <>
+            <button className="font-normal">
+                {props.title}
+            </button>
+        </>
+    )
+}
+
+export default Link;
